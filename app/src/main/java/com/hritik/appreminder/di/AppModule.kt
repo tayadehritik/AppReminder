6d6fda0171af355e5ec72c14ca60dba40e2c777a
tayadehritik <tayadehritik@gmail.com>
2024-11-14 02:58:26 +0530

    Done - Grant Usage Stats Permission

--- app/src/main/java/com/hritik/appreminder/di/AppModule.kt
+++ app/src/main/java/com/hritik/appreminder/di/AppModule.kt
@@ -1,8 +1,10 @@
 package com.hritik.appreminder.di
 
+
 import android.app.Application
+import android.app.usage.UsageStatsManager
+import android.content.Context
 import androidx.room.Room
-import androidx.room.RoomDatabase
 import com.hritik.appreminder.data.AppsDatabase
 import dagger.Module
 import dagger.Provides
@@ -22,4 +24,11 @@ object AppModule {
             AppsDatabase::class.java, "apps-database"
         ).build()
     }
+
+    @Provides
+    @Singleton
+    fun providesUsageStatsManager(app:Application): UsageStatsManager {
+        return app.getSystemService(Context.USAGE_STATS_SERVICE) as UsageStatsManager
+    }
+
 }
--- app/src/main/java/com/hritik/appreminder/ui/MainActivity.kt
+++ app/src/main/java/com/hritik/appreminder/ui/MainActivity.kt
@@ -2,27 +2,27 @@ package com.hritik.appreminder.ui
 
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Build
 import android.os.Bundle
+import android.provider.Settings
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.get
 import androidx.lifecycle.lifecycleScope
 import com.hritik.appreminder.ui.theme.AppReminderTheme
 import com.hritik.appreminder.viewmodel.MainViewModel
@@ -45,19 +45,27 @@ class MainActivity : ComponentActivity() {
         setContent {
             AppReminderTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    println(innerPadding)
                     val trackedPackages by mainViewModel.trackedPackages.collectAsState()
-                    LazyColumn {
-                        items(installedPackages) { installedPackage ->
-                            Row {
-                                Text(installedPackage)
-                                Checkbox(
-                                    checked = installedPackage in trackedPackages,
-                                    onCheckedChange = { value ->
-                                        if(value) mainViewModel.addPackage(installedPackage)
-                                        else mainViewModel.removePackage(installedPackage)
-                                    }
-                                )
+                    val usageStatsPermissionGranted by mainViewModel.usageStatsPermissionGranted.collectAsState()
+                    Column(modifier = Modifier.padding(innerPadding)) {
+                        Button(
+                            onClick = { grantUsageStatsPermission() },
+                            enabled = usageStatsPermissionGranted.not()
+                        ) {
+                            Text("Grant Usage Stats Permission")
+                        }
+                        LazyColumn {
+                            items(installedPackages) { installedPackage ->
+                                Row {
+                                    Text(installedPackage)
+                                    Checkbox(
+                                        checked = installedPackage in trackedPackages,
+                                        onCheckedChange = { value ->
+                                            if(value) mainViewModel.addPackage(installedPackage)
+                                            else mainViewModel.removePackage(installedPackage)
+                                        }
+                                    )
+                                }
                             }
                         }
                     }
@@ -78,6 +86,18 @@ class MainActivity : ComponentActivity() {
         } else {
             packageManager.queryIntentActivities(mainIntent, 0)
         }.map { it.activityInfo.packageName }
+    }
+
+    private fun grantUsageStatsPermission() {
+        val usageAccessIntent = Intent(Settings.ACTION_USAGE_ACCESS_SETTINGS).apply {
+            data = Uri.fromParts("package", packageName, null)
+        }
+        startActivity(usageAccessIntent)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mainViewModel.checkUsageStatsPermissionGranted()
     }
 }
 
